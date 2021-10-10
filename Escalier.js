5fa4bc5b7dc26325125c8eb6b8384fb3d43aa26e
Matheos Romain <romain.matheos.31@gmail.com>
2021-10-10 15:50:16 +0200

    Ajout d'un commentaire d'explication et d'une condition pour gérer la gestion d'erreur.

--- Escalier.js
+++ Escalier.js
@@ -1,4 +1,9 @@
 #! node
+/* Afficher un escalier ayant comme nombre de marche, le nombre inscrit en argument */
+
+if (isNaN(process.argv[2])) {
+  console.log("Veuillez indiquer un nombre en tant qu'argument au script. Ex : $> Escalier.js 5");
+}
 
 let nb = process.argv[2];
 let i = 0;
