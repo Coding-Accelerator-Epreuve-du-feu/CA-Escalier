6681587f12ace685a6b55b6399c1bf190c648de4
Matheos Romain <romain.matheos.31@gmail.com>
2021-10-17 15:23:09 +0200

    Maj gestion d'erreur

--- Escalier.js
+++ Escalier.js
@@ -1,14 +1,25 @@
 #! node
 /* Afficher un escalier ayant comme nombre de marche, le nombre inscrit en argument */
 
-if (isNaN(process.argv[2])) {
-  console.log("Veuillez indiquer un nombre en tant qu'argument au script. Ex : $> Escalier.js 5");
-}
-
 let nb = process.argv[2];
 let i = 0;
 
-while (i <= nb) {
-  console.log(" ".repeat(nb - i),"#".repeat(i));
-  i++;
+function escalier(nb) {
+
+if (isNaN(process.argv[2])) {
+  console.log("Veuillez indiquer au script, un nombre en argument. Ex : $> node Escalier.js 5");
 }
+
+else if (process.argv[3] !== undefined) {
+  console.log("Veuillez indiquer au script, qu'un seul nombre en argument. Ex : $> node Escalier.js 5");
+}
+
+else {
+  while (i <= nb) {
+    console.log(" ".repeat(nb - i),"#".repeat(i));
+    i++;
+}
+}
+}
+
+escalier(nb);
